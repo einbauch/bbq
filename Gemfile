bbb5be3335fea83d1b3e6c235366b4b8f82906d3
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-06-13 02:54:20 +0300

    bootstrap installed

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '~> 4.2.6'
 
+gem 'twitter-bootstrap-rails'
+
 gem 'uglifier', '>= 1.3.0'
 
 gem 'jquery-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :events
+  
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, length: {maximum: 225}
+  validates :email, uniqueness: true
+  validates :email, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
