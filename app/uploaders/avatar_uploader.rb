33fa0c21f35e56ce369e4c9fd0b36ce1543f6944
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-06-16 21:28:57 +0300

    event thumb pics randomized and img tag in html photo mailer

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -29,7 +29,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [400, 400]
+  process resize_to_fill: [300, 300]
   # process :scale => [200, 300]
   #
   # def scale(width, height)
