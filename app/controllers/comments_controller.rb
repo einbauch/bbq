e46e2a0b6ddc1952d0a28613beb59bf10743d279
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-06-16 17:58:14 +0300

    comment author should not recieve email about his comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -51,6 +51,8 @@ class CommentsController < ApplicationController
     # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
+    all_emails =- current_user.email if user_signed_in?
+
     # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
     # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
     all_emails.each do |mail|
