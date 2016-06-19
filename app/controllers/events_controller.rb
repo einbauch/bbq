1fecfd059d4126e1478c09fe761b6909bb715748
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-06-19 23:17:39 +0300

    pincode to events added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,6 +11,8 @@ class EventsController < ApplicationController
   # задаем объект @event от текущего юзера
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
+  before_action :pincode_guard, only: [:show]
+
   # GET /events
   def index
     @events = Event.all
@@ -61,6 +63,22 @@ class EventsController < ApplicationController
   end
 
   private
+
+  def pincode_guard
+    return if @event.pincode.blank?
+    return if user_signed_in? && current_user == @event.user
+
+    if params[:pincode].present? && @event.pincode == params[:pincode]
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    if cookies.permanent["events_#{@event.id}_pincode"] != @event.pincode
+      flash.now[:alert] = "Неправильный пинкод" if params[:pincode].present?
+      render 'pincode_form'
+    end
+
+  end
+
   def set_current_user_event
     @event = current_user.events.find(params[:id])
   end
@@ -70,6 +88,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description)
+    params.require(:event).permit(:title, :address, :datetime, :description, :pincode)
   end
 end
