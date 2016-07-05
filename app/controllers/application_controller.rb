7109df1e7fcee0fb0157277bc83379bd3c8572b9
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-07-05 09:21:50 +0300

    add name field to registraation form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # настройка для работы девайза при правке профиля юзера
+  # настройка для работы девайза при регистрации и правке профиля юзера
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # хелпер метод, доступный во вьюхах
   helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
+
+    # Разрешаем использование имени пользователя на странице регистрации
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    # Разрешенные параметры на странице правки профиля
     devise_parameter_sanitizer.for(:account_update) { |u|
       u.permit(:password, :password_confirmation, :current_password)
     }
